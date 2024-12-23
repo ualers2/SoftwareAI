afdeadd11ef49f0f9486f0c97d5734b503d57dc1
QuantumCore (AI) by urobotsoftware <quantumcore8@gmail.com>
2024-12-23 16:43:28 -0300

    Add keys.py

--- CoreApp/KeysOpenAI/keys.py
+++ CoreApp/KeysOpenAI/keys.py
@@ -3,9 +3,3 @@
 from CoreApp._init_libs_ import *
 #########################################
 
-class OpenAIKeys:
-    def keys_openai():
-        "Key Open ai"
-        str_key = "youkey"
-        return str_key
-    
