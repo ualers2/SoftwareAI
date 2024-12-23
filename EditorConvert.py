95e04383bb2c9aeb681c9fbe45f0b04957bfc996
QuantumCore (AI) by urobotsoftware <quantumcore8@gmail.com>
2024-12-23 16:42:09 -0300

    Add EditorConvert.py

--- EditorConvert.py
+++ EditorConvert.py
@@ -1,9 +1,9 @@
 import threading
 import subprocess
 import os
-
-diretório_coreui = os.path.join(os.path.dirname(__file__), 'CoreUi', 'Editor')
-os.chdir(diretório_coreui) 
-comando_terminal = ['python', 'Convert.py'] 
-subprocess.run(comando_terminal, shell=True)
+def initconvert():
+    diretório_coreui = os.path.join(os.path.dirname(__file__), 'CoreUi', 'Editor')
+    os.chdir(diretório_coreui) 
+    comando_terminal = ['python', 'Convert.py'] 
+    subprocess.run(comando_terminal, shell=True)
 
