118c0d3d83218b423ce496d78ba53742e1b22a85
QuantumCore (AI) by urobotsoftware <quantumcore8@gmail.com>
2024-12-22 22:54:28 -0300

    Add QProcessCreateGitHubKeys.py

---
+++ CoreUi/Editor/QProcess/QProcessCreateGitHubKeys.py
@@ -0,0 +1,60 @@
+########################################################################
+## IMPORTS Libs
+import sys
+import json
+import time
+import os
+import subprocess
+import platform
+from firebase_admin import credentials, initialize_app, storage, db, delete_app
+import concurrent.futures
+########################################################################
+
+########################################################################
+# IMPORT Pyside2
+from PySide2extn.RoundProgressBar import roundProgressBar #IMPORT THE EXTENSION LIBRARY
+from PySide2.QtCore import QTimer, Signal, QThread
+from PySide2.QtWidgets import QFileDialog
+########################################################################
+
+from CoreApp._init_core_ import OpenAIKeysinit,AutenticateAgent,Agent_files,  ResponseAgent, python_functions 
+# IMPORT SoftwareAI Keys 
+from CoreApp._init_keys_ import *
+#########################################
+
+class QProcessCreateGitHubKeys(QThread):
+    ModalSucess = Signal(str)
+    ModalInfo = Signal(str)
+
+    def __init__(self,
+                githuusername_AgentKeys,
+                githubtoken_AgentKeys
+            ):
+        super().__init__()
+        self.githuusername_AgentKeys = githuusername_AgentKeys
+        self.githubtoken_AgentKeys = githubtoken_AgentKeys
+
+    def run(self):
+
+
+        PATH_caminho = os.path.abspath(os.path.join(os.path.dirname(__file__), f'../../../CoreApp/KeysGitHub'))
+        file_path = os.path.join(PATH_caminho, f"keys.py")
+        with open(file_path, 'a', encoding='utf-8') as file:
+            file.write(F'''
+
+
+class GithubKeys{self.githuusername_AgentKeys.replace(" ", "_")}:
+                       
+    def {self.githuusername_AgentKeys.replace(" ", "_")}_github_keys():
+        github_username = "{self.githuusername_AgentKeys}"
+        github_token = "{self.githubtoken_AgentKeys}"
+        return github_username, github_token
+
+
+
+            ''')
+            file.close()
+
+
+        self.ModalSucess.emit(f"Your Key has been created")
+
