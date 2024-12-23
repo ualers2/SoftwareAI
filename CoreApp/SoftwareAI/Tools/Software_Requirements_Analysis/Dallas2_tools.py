ea009ea3e71068cb32b2696514234d967f189743
QuantumCore (AI) by urobotsoftware <quantumcore8@gmail.com>
2024-12-23 00:11:53 -0300

    Add Dallas2_tools.py

---
+++ CoreApp/SoftwareAI/Tools/Software_Requirements_Analysis/Dallas2_tools.py
@@ -0,0 +1,20 @@
+
+
+tools_Dallas2 = [
+{"type": "file_search"},    
+{
+
+        "type": "function",
+            "function": {
+            "name": "pegar_hora",
+            "description": "Retorna a data e hora atual no formato YYYY-MM-DD HH:MM:SS.",
+            "parameters": {
+            "type": "object",
+            "properties": {},
+            "required": []
+            }
+        }
+ } 	
+]
+
+                
