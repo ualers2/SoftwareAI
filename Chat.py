cae18b739e43149804a78f34cc73dc9322150305
QuantumCore (AI) by urobotsoftware <quantumcore8@gmail.com>
2024-12-23 16:42:06 -0300

    Add Chat.py

---
+++ Chat.py
@@ -0,0 +1,8 @@
+import threading
+import subprocess
+import os
+def initchat():
+    diretório_coreui = os.path.join(os.path.dirname(__file__), 'CoreUi', 'Chat')
+    os.chdir(diretório_coreui)  # Mudando para o diretório 
+    comando_terminal = ['python', 'main.py']  # Executando main.py dentro do diretório 
+    subprocess.Popen(comando_terminal, shell=True)
