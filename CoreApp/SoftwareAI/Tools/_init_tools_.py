c07c2886876c18d01cc570e95e1b27421b91e6d8
QuantumCore (AI) by urobotsoftware <quantumcore8@gmail.com>
2024-12-30 16:57:41 -0300

    Add _init_tools_.py

--- CoreApp/SoftwareAI/Tools/_init_tools_.py
+++ CoreApp/SoftwareAI/Tools/_init_tools_.py
@@ -1,14 +1,16 @@
-from .Company_CEO.ByteManager_tools import *
-from .Company_Managers.MatrixMinder_tools import *
-from .Pre_Project.Tigrao_tools import *
-from .Software_Development.DataWeaver_tools import *
-from .Software_Development.QuantumCore_tools import *
-from .Software_Development.NexGenCoder_tools import *
-from .Software_Development.SignalMaster_tools import *
-from .Software_Documentation.CloudArchitect_tools import *
-from .Software_Planning.Bob_tools import *
-from .Software_Planning.Dallas_tools import *
-from .Software_Requirements_Analysis.SynthOperator_tools import *
+from softwareai.CoreApp.SoftwareAI.Tools.Company_CEO.ByteManager_tools import *
+from softwareai.CoreApp.SoftwareAI.Tools.Company_Managers.MatrixMinder_tools import *
+from softwareai.CoreApp.SoftwareAI.Tools.Pre_Project.Tigrao_tools import *
+from softwareai.CoreApp.SoftwareAI.Tools.Software_Development.DataWeaver_tools import *
+from softwareai.CoreApp.SoftwareAI.Tools.Software_Development.QuantumCore_tools import *
+from softwareai.CoreApp.SoftwareAI.Tools.Software_Development.NexGenCoder_tools import *
+from softwareai.CoreApp.SoftwareAI.Tools.Software_Development.SignalMaster_tools import *
+from softwareai.CoreApp.SoftwareAI.Tools.Software_Documentation.CloudArchitect_tools import *
+from softwareai.CoreApp.SoftwareAI.Tools.Software_Planning.Bob_tools import *
+from softwareai.CoreApp.SoftwareAI.Tools.Software_Planning.Dallas_tools import *
+from softwareai.CoreApp.SoftwareAI.Tools.Software_Requirements_Analysis.SynthOperator_tools import *
 
-from .Software_Requirements_Analysis.Dallas2_tools import *
+                
+                
+from softwareai.CoreApp.SoftwareAI.Tools.Software_Requirements_Analysis.ByteManager_tools import *
                 
