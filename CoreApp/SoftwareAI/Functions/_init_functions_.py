a04d0dd33531990d5bc5ba8bf9fd2087b97e08c9
QuantumCore (AI) by urobotsoftware <quantumcore8@gmail.com>
2024-12-30 16:56:37 -0300

    Add _init_functions_.py

--- CoreApp/SoftwareAI/Functions/_init_functions_.py
+++ CoreApp/SoftwareAI/Functions/_init_functions_.py
@@ -1,14 +1,14 @@
-from .get_datetime_function import *
-from .run_testes_function import *
-from .create_github_function import *
-from .improve_code_function import *
-from .review_pr_function import *
-from .add_project_map_function import *
-from .analyze_file_function import *
-from .save_TXT_function import *
-from .save_code_function import *
-from .execute_python_code_function import *
-from .update_readme_function import *
+from softwareai.CoreApp.SoftwareAI.Functions.get_datetime_function import *
+from softwareai.CoreApp.SoftwareAI.Functions.run_testes_function import *
+from softwareai.CoreApp.SoftwareAI.Functions.create_github_function import *
+from softwareai.CoreApp.SoftwareAI.Functions.improve_code_function import *
+from softwareai.CoreApp.SoftwareAI.Functions.review_pr_function import *
+from softwareai.CoreApp.SoftwareAI.Functions.add_project_map_function import *
+from softwareai.CoreApp.SoftwareAI.Functions.analyze_file_function import *
+from softwareai.CoreApp.SoftwareAI.Functions.save_TXT_function import *
+from softwareai.CoreApp.SoftwareAI.Functions.save_code_function import *
+from softwareai.CoreApp.SoftwareAI.Functions.execute_python_code_function import *
+from softwareai.CoreApp.SoftwareAI.Functions.update_readme_function import *
             
-from .pegar_hora_function import *
+from softwareai.CoreApp.SoftwareAI.Functions.pegar_hora_function import *
                 
