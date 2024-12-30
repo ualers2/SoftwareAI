5c8824fbe86fb105b5dbb6e49ffbb0acfb2170af
QuantumCore (AI) by urobotsoftware <quantumcore8@gmail.com>
2024-12-30 17:09:55 -0300

    Add exemple_1.py

--- Examples/exemple_1.py
+++ Examples/exemple_1.py
@@ -1,14 +1,12 @@
 #########################################
 # IMPORT SoftwareAI Agents
-from CoreApp._init_agents_ import AgentInitializer
+from softwareai.CoreApp._init_agents_ import AgentInitializer
 #########################################
 # IMPORT SoftwareAI Libs 
-from CoreApp._init_libs_ import *
+from softwareai.CoreApp._init_libs_ import *
 #########################################
 
-
 byte_manager = AgentInitializer.get_agent('ByteManager') 
-
-mensagem = "solicito um script para Análise técnica da criptomoeda xrp  "
+mensagem = "solicito um script para Análise técnica da criptomoeda dogecoin"
 owner_response = byte_manager.AI_1_ByteManager_Company_Owners(mensagem)
 print(owner_response)
