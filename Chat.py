e0f84a95e0acb01e5490e0ed5d8374895a346113
QuantumCore (AI) by urobotsoftware <quantumcore8@gmail.com>
2024-12-30 17:02:40 -0300

    Add Chat.py

--- Chat.py
+++ Chat.py
@@ -6,3 +6,4 @@ def initchat():
     os.chdir(diretório_coreui)  # Mudando para o diretório 
     comando_terminal = ['python', 'main.py']  # Executando main.py dentro do diretório 
     subprocess.Popen(comando_terminal, shell=True)
+initchat()
