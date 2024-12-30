b43458b5783c2fdcd1a7e310e4dd1b5360b1f3ad
QuantumCore (AI) by urobotsoftware <quantumcore8@gmail.com>
2024-12-30 17:06:08 -0300

    Add _init_tools_.py

---
+++ CoreApp/SoftwareAI/Tools/_init_tools_.py
@@ -0,0 +1,16 @@
+from softwareai.CoreApp.SoftwareAI.Tools.Company_CEO.ByteManager_tools import *
+from softwareai.CoreApp.SoftwareAI.Tools.Company_Managers.MatrixMinder_tools import *
+from softwareai.CoreApp.SoftwareAI.Tools.Pre_Project.Tigrao_tools import *
+from softwareai.CoreApp.SoftwareAI.Tools.Software_Development.DataWeaver_tools import *
+from softwareai.CoreApp.SoftwareAI.Tools.Software_Development.QuantumCore_tools import *
+from softwareai.CoreApp.SoftwareAI.Tools.Software_Development.NexGenCoder_tools import *
+from softwareai.CoreApp.SoftwareAI.Tools.Software_Development.SignalMaster_tools import *
+from softwareai.CoreApp.SoftwareAI.Tools.Software_Documentation.CloudArchitect_tools import *
+from softwareai.CoreApp.SoftwareAI.Tools.Software_Planning.Bob_tools import *
+from softwareai.CoreApp.SoftwareAI.Tools.Software_Planning.Dallas_tools import *
+from softwareai.CoreApp.SoftwareAI.Tools.Software_Requirements_Analysis.SynthOperator_tools import *
+
+                
+                
+from softwareai.CoreApp.SoftwareAI.Tools.Software_Requirements_Analysis.ByteManager_tools import *
+                
