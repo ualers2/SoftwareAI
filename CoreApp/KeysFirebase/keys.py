062004938f24ef2a4eb4ee588601fc705ef9d02c
ualers <125712906+ualers@users.noreply.github.com>
2024-12-30 17:03:28 -0300

    Delete CoreApp directory

--- CoreApp/KeysFirebase/keys.py
+++
@@ -1,5 +0,0 @@
-#########################################
-# IMPORT SoftwareAI Libs 
-from softwareai.CoreApp._init_libs_ import *
-#########################################
-
--- CoreApp/KeysOpenAI/keys.py
+++
@@ -1,6 +0,0 @@
-#########################################
-# IMPORT SoftwareAI Libs 
-from softwareai.CoreApp._init_libs_ import *
-#########################################
-
-            
