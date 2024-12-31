295be320fc1e956ac6a60b833091812e11cc7f5d
ualers <125712906+ualers@users.noreply.github.com>
2024-12-31 17:43:14 -0300

    Add _init_core_.py

---
+++ _init_core_.py
@@ -0,0 +1,10 @@
+from softwareai.CoreApp.SoftwareAI.Core import (
+        AutenticateAgent,
+        ResponseAgent,
+        python_functions,
+        Agent_files,
+        Agent_files_update,
+        Agent_destilation,
+        OpenAIKeysinit,
+        FirebaseKeysinit
+)
