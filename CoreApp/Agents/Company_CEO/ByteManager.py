096b463eb068a269d4a4fbaa498e422de8e39ebc
ualers <125712906+ualers@users.noreply.github.com>
2025-01-14 12:27:44 -0300

    Add ByteManager.py

--- CoreApp/Agents/Company_CEO/ByteManager.py
+++ CoreApp/Agents/Company_CEO/ByteManager.py
@@ -31,6 +31,7 @@ class ByteManager:
                 Equipe_De_Solucoes,
                 Softwareanaysis,
                 SoftwareDevelopment,
+                Software_Documentation
                 ):
         
         self.Company_Managers = Company_Managers
@@ -39,40 +40,32 @@ class ByteManager:
         self.Equipe_De_Solucoes = Equipe_De_Solucoes
         self.Softwareanaysis = Softwareanaysis
         self.SoftwareDevelopment = SoftwareDevelopment
+        self.Software_Documentation = Software_Documentation
 
 
 
 
     
-    def AI_1_ByteManager_Company_Owners(self,mensagem):
-        """Nome da IA: ByteManager \n
-        Função: Dono de Empresa de Software \n
-        Horario de trabalho: 2H
-        
-        """    
-
+    def AI_1_ByteManager_Company_Owners(self, 
+                                        mensagem,
+                                        appfb,
+                                        vectorstore_in_assistant = None,
+                                        vectorstore_in_Thread = None,
+                                        Upload_1_file_in_thread = None,
+                                        Upload_1_file_in_message = None,
+                                        Upload_1_image_for_vision_in_thread = None,
+                                        Upload_list_for_code_interpreter_in_thread = None
+                                        
+                                        ):
 
         key = "AI_ByteManager_Company_Owners"
         nameassistant = "AI ByteManager Donos da Empresa Urobotsoftware"
         model_select = "gpt-4o-mini-2024-07-18"
 
-
-        vectorstore_in_assistant = None #[ 
-        #             "vs_USBolYuyy7cVXhfBWFToQcaN"
-        #         ]
-        vectorstore_in_Thread = None
-
-        
-        Upload_1_file_in_thread = None
-        Upload_1_file_in_message = None
-        Upload_1_image_for_vision_in_thread = None
-        Upload_list_for_code_interpreter_in_thread = None
-
         key_openai = OpenAIKeysteste.keys()
-        name_app = "appx"
-        appfb = FirebaseKeysinit._init_app_(name_app)
         client = OpenAIKeysinit._init_client_(key_openai)
 
+        github_username, github_token = GithubKeys.ByteManagerCompanyOwner_github_keys()
 
         AI_ByteManager, instructionsassistant, nameassistant, model_select = AutenticateAgent.create_or_auth_AI(appfb, client, key, instructionByteManager, nameassistant, model_select, tools_ByteManager, vectorstore_in_assistant)
 
@@ -80,9 +73,9 @@ class ByteManager:
         
         """  
 
-        exemplo = "Caso seja solicitado algum script ou software Responda no formato JSON Exemplo: {'solicitadoalgumcodigo': 'solicitacao...'} "
+
             
-        mensaxgemfinal = mensaxgem + exemplo
+        mensaxgemfinal = mensaxgem 
         
         response, total_tokens, prompt_tokens, completion_tokens = ResponseAgent.ResponseAgent_message_with_assistants(
                                                                 mensagem=mensaxgemfinal,
@@ -92,10 +85,9 @@ class ByteManager:
                                                                 client=client,
                                                                 tools=tools_ByteManager,
                                                                 model_select=model_select,
-                                                                aditional_instructions=adxitional_instructions_ByteManager)
+                                                                aditional_instructions=adxitional_instructions_ByteManager
+                                                                )
                                                 
-                                
-         
                                             
         ##Agent Destilation##                   
         Agent_destilation.DestilationResponseAgent(mensaxgemfinal, response, instructionsassistant, nameassistant)
@@ -128,7 +120,21 @@ class ByteManager:
             resposta_AI_ByteManager_dict = json.loads(passando_resposta_do_matrixminder)
             resposta_AI_ByteManager = resposta_AI_ByteManager_dict['resposta']
             return resposta_AI_ByteManager
-        
+
+        if 'solicitadoatualizaçãoderepositoriosoftware' in teste_dict:
+
+            repo_name = teste_dict['solicitadoatualizaçãoderepositoriosoftware'] 
+
+            init_env(repo_name)
+
+            Melhorias = self.SoftwareDevelopment.QuantumCoreUpdate(
+                appfb, client, repo_name
+                )
+            
+            CloudArchitectUpdateReadme = self.Software_Documentation.CloudArchitectUpdateReadme(
+                appfb, client, repo_name, Melhorias
+            )
+
         if 'solicitadoalgumcodigo' in teste_dict:
 
 
@@ -170,34 +176,61 @@ class ByteManager:
                 repo_name = response["nome"]
                 print(repo_name)
                 repo_name_str = f"{repo_name}"
-                repo_name_replace = repo_name_str.replace("-", "")
+                repo_name_replace = repo_name_str.replace("-", "").replace("A-I-O-R-G-", "").replace("A-I-O-R-G", "").replace("A-I-O-R-G/", "")
                 print(repo_name_replace)
                 repo_name = repo_name_replace
             except Exception as errror2:
                 print(errror2)
                 print(response)
-                                                
+            
             ##Agent Destilation##                   
             Agent_destilation.DestilationResponseAgent(mensagem, response, instructionsassistant, nameassistant)
             
+            mensaxgem = f"""crie uma descricao de 250 caracteres para o repositorio no github com base na descricao:\n
+            {pergunta_ao_tigrao}
+            """
+            format = 'Responda no formato JSON Exemplo: {"descricao": "descricao..."}'
+            mensagem = mensaxgem + format
+            response = ResponseAgent.ResponseAgent_message_completions(mensagem, key_openai, "regra descricao não execeder 250 caracteres", True, True)
+            repo_description = response["descricao"]
+
+            ##Agent Destilation##                   
+            Agent_destilation.DestilationResponseAgent(mensagem, response, instructionsassistant, nameassistant)
+
+                                                
+            mensaxgemrepositorio = f"Realize a criacao do repositorio no github usando (create_repo)\n repo_name:\n {repo_name}\ndescription:\n {repo_description}\n token:\n {github_token}\n"
+            response, total_tokens, prompt_tokens, completion_tokens = ResponseAgent.ResponseAgent_message_with_assistants(
+                                                                mensagem=mensaxgemrepositorio,
+                                                                agent_id=AI_ByteManager, 
+                                                                key=key,
+                                                                app1=appfb,
+                                                                client=client,
+                                                                tools=tools_ByteManager,
+                                                                model_select=model_select,
+                                                                aditional_instructions=adxitional_instructions_ByteManager
+                                                                )
+
+            ##Agent Destilation##                   
+            Agent_destilation.DestilationResponseAgent(mensaxgemrepositorio, response, instructionsassistant, nameassistant)
+            
             init_env(repo_name)
 
-            resposta_do_tigrao_com_doc_pre_projeto = self.Pre_Project_Document.AI_1_Pre_Project_Document_Writer(appfb, client, pergunta_ao_tigrao)#invoke_tigrao(pergunta_ao_tigrao)
+            resposta_do_tigrao_com_doc_pre_projeto = self.Pre_Project_Document.AI_1_Pre_Project_Document_Writer(appfb, client, pergunta_ao_tigrao, repo_name)#invoke_tigrao(pergunta_ao_tigrao)
 
             print(resposta_do_tigrao_com_doc_pre_projeto)
 
             Uploadfile = resposta_do_tigrao_com_doc_pre_projeto
 
-            path_planilha_Projeto, path_nome_do_cronograma, path_name_doc_Pre_Projeto = self.Gerente_de_projeto.Bob_Gerente_de_projeto(appfb, client, Uploadfile)
+            path_planilha_Projeto, path_nome_do_cronograma, path_name_doc_Pre_Projeto = self.Gerente_de_projeto.Bob_Gerente_de_projeto(appfb, client, Uploadfile, repo_name)
             
-            path_Roadmap, cronograma_do_projeto, planilha, doc_Pre_Projeto = self.Equipe_De_Solucoes.Dallas_Equipe_De_Solucoes_Roadmap(appfb, client, path_nome_do_cronograma, path_planilha_Projeto, path_name_doc_Pre_Projeto)
+            path_Roadmap, cronograma_do_projeto, planilha, doc_Pre_Projeto = self.Equipe_De_Solucoes.Dallas_Equipe_De_Solucoes_Roadmap(appfb, client, path_nome_do_cronograma, path_planilha_Projeto, path_name_doc_Pre_Projeto, repo_name)
             
             # file_paths_to_project = [f"{path_Roadmap}", f"{cronograma_do_projeto}", f"{planilha}", f"{doc_Pre_Projeto}"]
             # print(file_paths_to_project)
             # vectorstore_id = Agent_files.auth_or_create_vectorstore("project_", file_paths_to_project)
             # print(vectorstore_id)
 
-            anaysis_in_txt_path = self.Softwareanaysis.AI_SynthOperator(appfb, client, path_Roadmap, cronograma_do_projeto, planilha, doc_Pre_Projeto)
+            anaysis_in_txt_path = self.Softwareanaysis.AI_SynthOperator(appfb, client, path_Roadmap, cronograma_do_projeto, planilha, doc_Pre_Projeto, repo_name)
 
             script_version_1_path = self.SoftwareDevelopment.AI_QuantumCore(
                     appfb, client, repo_name,
