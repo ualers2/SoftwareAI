f6393748b32285df46273d465b4f3a27e16bdb69
ualers <125712906+ualers@users.noreply.github.com>
2025-01-14 12:28:47 -0300

    Add autosave_function.py

---
+++ CoreApp/SoftwareAI/Functions/autosave_function.py
@@ -0,0 +1,23 @@
+
+#########################################
+# IMPORT SoftwareAI Libs 
+from softwareai.CoreApp._init_libs_ import *
+#########################################
+
+def autosave(code, path):
+    """
+    Save the provided Python code string to a file.
+
+    Parameters:
+    ----------
+    code (str): The Python code to save.
+    path (str): The name of the file where the code will be saved.
+
+    Returns:
+    -------
+    None
+    """
+    with open(path, 'w', encoding="utf-8") as file:
+        file.write(code)
+
+    return True
