0a1ae7bdfa2ceb6093b4ae31e151c89579cc0929
ualers <125712906+ualers@users.noreply.github.com>
2025-01-14 12:29:35 -0300

    Add _init_submit_outputs_.py

--- CoreApp/SoftwareAI/Functions_Submit_Outputs/_init_submit_outputs_.py
+++ CoreApp/SoftwareAI/Functions_Submit_Outputs/_init_submit_outputs_.py
@@ -1,7 +1,7 @@
 #from softwareai.CoreApp.SoftwareAI.Functions_Submit_Outputs._submit_outputs import submit_output_
 from softwareai.CoreApp.SoftwareAI.Functions_Submit_Outputs.pegar_hora_submit_outputs import submit_output_pegar_hora
 
-from softwareai.CoreApp.SoftwareAI.Functions_Submit_Outputs.create_github_repo_and_upload_submit_outputs import submit_output_create_github_repo_and_upload
+from softwareai.CoreApp.SoftwareAI.Functions_Submit_Outputs.upload_project_py_submit_outputs import submit_output_upload_project_py
 from softwareai.CoreApp.SoftwareAI.Functions_Submit_Outputs.get_current_datetime_submit_outputs import submit_output_get_current_datetime
 from softwareai.CoreApp.SoftwareAI.Functions_Submit_Outputs.improve_code_and_create_pull_request_submit_outputs import submit_output_improve_code_and_create_pull_request
 from softwareai.CoreApp.SoftwareAI.Functions_Submit_Outputs.test_software_submit_outputs import submit_output_test_software
@@ -9,8 +9,11 @@ from softwareai.CoreApp.SoftwareAI.Functions_Submit_Outputs.add_project_map_subm
 from softwareai.CoreApp.SoftwareAI.Functions_Submit_Outputs.analyze_file_outputs import submit_output_analyze_file
 from softwareai.CoreApp.SoftwareAI.Functions_Submit_Outputs.save_TXT_outputs import submit_output_save_TXT
 from softwareai.CoreApp.SoftwareAI.Functions_Submit_Outputs.save_code_outputs import submit_output_save_code
-from softwareai.CoreApp.SoftwareAI.Functions_Submit_Outputs.execute_python_code_outputs import submit_output_execute_python_code
+from softwareai.CoreApp.SoftwareAI.Functions_Submit_Outputs.execute_py_outputs import submit_output_execute_py
 from softwareai.CoreApp.SoftwareAI.Functions_Submit_Outputs.update_readme_outputs import submit_output_update_readme
+from softwareai.CoreApp.SoftwareAI.Functions_Submit_Outputs.autosave_submit_outputs import submit_output_autosave
+from softwareai.CoreApp.SoftwareAI.Functions_Submit_Outputs.autoupload_submit_outputs import submit_output_autoupload
+from softwareai.CoreApp.SoftwareAI.Functions_Submit_Outputs.create_repo_submit_outputs import submit_output_create_repo
 
 def _init_output_(function_name,
                 function_arguments,
@@ -22,15 +25,18 @@ def _init_output_(function_name,
     functions_to_call = [
         submit_output_pegar_hora,
         submit_output_get_current_datetime,
-        submit_output_create_github_repo_and_upload,
+        submit_output_upload_project_py,
         submit_output_improve_code_and_create_pull_request,
         submit_output_test_software,
         submit_output_add_projectmap_to_github,
         submit_output_analyze_file,
         submit_output_save_TXT,
         submit_output_save_code,
-        submit_output_execute_python_code,
-        submit_output_update_readme
+        submit_output_execute_py,
+        submit_output_update_readme,
+        submit_output_autosave,
+        submit_output_autoupload,
+        submit_output_create_repo
     ]
     
     for func in functions_to_call:
