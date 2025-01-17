ffa68f9decb66388f2f4f900fca2c4fb8597403e
ualers <125712906+ualers@users.noreply.github.com>
2025-01-17 13:23:27 -0300

    Add _init_Instructions_.py

---
+++ CoreApp/_init_Instructions_.py
@@ -0,0 +1,15 @@
+from softwareai.CoreApp.SoftwareAI.Instructions.Software_Planning.Bob import *
+from softwareai.CoreApp.SoftwareAI.Instructions.Company_CEO.ByteManager import *
+from softwareai.CoreApp.SoftwareAI.Instructions.Software_Documentation.CloudArchitect import *
+from softwareai.CoreApp.SoftwareAI.Instructions.Software_Planning.Dallas import *
+from softwareai.CoreApp.SoftwareAI.Instructions.Software_Development.DataWeaver import *
+from softwareai.CoreApp.SoftwareAI.Instructions.Company_Managers.MatrixMinder import *
+from softwareai.CoreApp.SoftwareAI.Instructions.Software_Development.DataWeaver import *
+from softwareai.CoreApp.SoftwareAI.Instructions.Software_Development.QuantumCore import *
+from softwareai.CoreApp.SoftwareAI.Instructions.Software_Development.NexGenCoder import *
+from softwareai.CoreApp.SoftwareAI.Instructions.Software_Development.SignalMaster import *
+from softwareai.CoreApp.SoftwareAI.Instructions.Software_Requirements_Analysis.SynthOperator import *
+from softwareai.CoreApp.SoftwareAI.Instructions.Pre_Project.Tigrao import *
+
+from softwareai.CoreApp.SoftwareAI.Instructions.Software_Requirements_Analysis.ByteManager import *
+                
