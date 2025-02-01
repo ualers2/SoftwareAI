b538feb29e4c495ec0327028385ca9e362052cd4
QuantumCore (AI) by urobotsoftware <quantumcore8@gmail.com>
2025-02-01 13:24:52 -0300

    Add KnowLedgeData.py

---
+++ CoreCompany/KnowLedgeData.py
@@ -0,0 +1,8 @@
+KnowLedgeData = {
+   "SoftwareAIDocs": "vs_tzkPCBqftVhy78ZbVBKAk5VS", 
+   "Books_Python": "vs_bWI8QgwrZRfMUs6sxRghNcUy", 
+   "GithubReadme": "vs_ua0NbKDlYfcmuzlj7eLWxZwa", 
+   "CompanyCodes": "vs_JcVA69jlQD0oyrmNSUIMrlqt", 
+   
+}
+
