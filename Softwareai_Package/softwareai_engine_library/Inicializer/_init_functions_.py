96a85b0423c01eb5a1a78354491a0a86163790ff
UALERSON <mediacutsstudio@gmail.com>
2025-05-06 12:55:58 -0300

    Add _init_functions_.py

---
+++ Softwareai_Package/softwareai_engine_library/Inicializer/_init_functions_.py
@@ -0,0 +1,25 @@
+
+# from softwareai_engine_library.FunctionsAndTools.Functions.autosave.autosave import *
+# from softwareai_engine_library.FunctionsAndTools.Functions.autoupload.autoupload import *
+# from softwareai_engine_library.FunctionsAndTools.Functions.autocreaterepo.autocreaterepo import *
+# from softwareai_engine_library.FunctionsAndTools.Functions.autopullrequest.autopullrequest import *
+# from softwareai_engine_library.FunctionsAndTools.Functions.autogetstructure.autogetstructure import *
+# from softwareai_engine_library.FunctionsAndTools.Functions.autogetfilecontent.autogetfilecontent import *
+# from softwareai_engine_library.FunctionsAndTools.Functions.autochangesrequestedinpullrequest.autochangesrequestedinpullrequest import *
+# from softwareai_engine_library.FunctionsAndTools.Functions.autoconversationpullrequest.autoconversationpullrequest import *
+# from softwareai_engine_library.FunctionsAndTools.Functions.autounittests.autounittests import *
+# from softwareai_engine_library.FunctionsAndTools.Functions.autocreateissue.autocreateissue import *
+# from softwareai_engine_library.FunctionsAndTools.Functions.autoapprovepullrequest.autoapprovepullrequest import *
+# from softwareai_engine_library.FunctionsAndTools.Functions.send_to_webhook_func.send_to_webhook_func import *
+# from softwareai_engine_library.FunctionsAndTools.Functions.criar_grafico.criar_grafico import *
+    
+      
+
+
+
+
+
+
+
+
+
